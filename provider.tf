ea433395bc4506e02696acf03cc1f847c466b45d
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-04-23 01:40:59 -0400

    removed deprecated features

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    region  = "${var.aws_region}"
+    region  = var.aws_region
     version = "~> 2.0"
-    shared_credentials_file = "${var.credentials}"
-    profile                 = "${var.profile}"
+    shared_credentials_file = var.credentials
+    profile                 = var.profile
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
-    type = "string"
+    type = string
     default = "us-east-1"
     description = "AWS region where cluster is to be constructed"
 }
 
 variable "credentials" {
-    type = "string"
+    type = string
     default = "~/.aws/credentials"
     description = "AWS access keys to use to login"
 }
 
 variable "profile" {
-    type = "string"
+    type = string
     default = "default"
     description = "AWS profile to use to login"
 }
