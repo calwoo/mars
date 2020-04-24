08846e0602511c7293f9c2678d46efd2d974369d
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-04-24 14:02:10 -0400

    formatted

--- cluster.tf
+++ cluster.tf
@@ -2,22 +2,28 @@
 # Cluster distributed state backend
 
 terraform {
-    backend "s3" {
-        region = "us-east-1"
-        bucket = "cwoo-generic"
-        key    = "terraform.tfstate"
-    }
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "cwoo-generic"
+    key    = "terraform.tfstate"
+  }
 }
 
 ###################
 # Cluster instances
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    description = "VPC for cluster"
+  }
 }
+
+
 
 
 
@@ -26,89 +32,16 @@ resource "aws_vpc" "main" {
 #########################
 # Cluster security groups
 
-resource "aws_security_group" "ec2-master" {
-    name = "ec2-cluster-master"
-    description = "Cluster master communication with worker nodes"
-    vpc_id = aws_vpc.main.id
+resource "aws_security_group" "ec2-cluster-sg" {
+  name        = "ec2-cluster"
+  description = "Cluster security group with basic permissions"
+  vpc_id      = aws_vpc.main.id
 
-    # SSH access
-    ingress {
-        from_port = 22
-        to_port   = 22
-        protocol = "tcp"
-        cidr_blocks = [ "${var.my_ip}/32" ]
-    }
-
-    # Jupyter notebook access
-    ingress {
-        from_port = 8888
-        to_port = 8888
-        protocol = "tcp"
-        cidr_blocks = [ "${var.my_ip}/32" ]
-    }
-
-    # HTTP access (for Git)
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-    # HTTPS access (for Git)
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-    # master <-> worker
-    ingress {
-        from_port = var.worker_port
-        to_port = var.master_port
-        protocol = "tcp"
-    }
-}
-
-resource "aws_security_group" "ec2-worker" {
-    name = "ec2-cluster-node"
-    description = "Cluster worker communication with master node"
-    vpc_id = aws_vpc.main.id
-
-    # HTTP access (for Git)
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-    # HTTPS access (for Git)
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-    # master <-> worker
-    ingress {
-        from_port = var.master_port
-        to_port = var.worker_port
-        protocol = "tcp"
-    }
-
-    # worker <-> worker
-    egress {
-        from_port = var.worker_port
-        to_port = var.worker_port
-        protocol = "tcp"
-    }
-
-    ingress {
-        from_port = var.worker_port
-        to_port = var.worker_port
-        protocol = "tcp"
-    }
+  # SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}/32"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
 variable "aws_region" {
-    type = string
-    default = "us-east-1"
-    description = "AWS region where cluster is to be constructed"
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region where cluster is to be constructed"
 }
 
 variable "credentials" {
-    type = string
-    default = "~/.aws/credentials"
-    description = "AWS access keys to use to login"
+  type        = string
+  default     = "~/.aws/credentials"
+  description = "AWS access keys to use to login"
 }
 
 variable "profile" {
-    type = string
-    default = "default"
-    description = "AWS profile to use to login"
+  type        = string
+  default     = "default"
+  description = "AWS profile to use to login"
 }
 
 ###################
 # Cluster variables
 
 variable "master_port" {
-    type = number
-    default = 1234
-    description = "Port of master node for distributed handshake"
+  type        = number
+  default     = 1234
+  description = "Port of master node for distributed handshake"
 }
 
 variable "master_ip_addr" {
-    type = string
-    default = "127.0.0.1"
-    description = "IP address of master node for distributed handshake"
+  type        = string
+  default     = "127.0.0.1"
+  description = "IP address of master node for distributed handshake"
 }
 
 variable "worker_port" {
-    type = number
-    default = 1235
-    description = "Port of worker nodes for distributed handshake"
+  type        = number
+  default     = 1235
+  description = "Port of worker nodes for distributed handshake"
 }
 
 variable "my_ip" {
-    type = string
-    description = "Your IP address"
+  type        = string
+  description = "Your IP address"
 }
