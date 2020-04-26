512f7d0cc4a473c4fec831841881fde5671d7df9
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-04-26 15:29:39 -0400

    starting init scripts for nodes

---
+++ scripts/master.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# log all user data invocations to console:
+# from https://aws.amazon.com/premiumsupport/knowledge-center/ec2-linux-log-user-data/
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
