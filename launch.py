bad9007b4fdbfe61ebb160f22bac5d3206f8cd36
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-04-29 17:03:00 -0400

    testing launch script

--- launch.py
+++ launch.py
@@ -5,11 +5,13 @@ This programmatically launches a PyTorch distributed training job on a custom EC
 cluster.
 """
 
-import sys
-import os
-import shutil
 import argparse
 import logging
+import multiprocessing as mp
+import os
+import shutil
+import sys
+from functools import partial
 
 from fabric import Connection
 from fabric.group import Group, ThreadingGroup
@@ -78,27 +80,41 @@ def load_artifacts(artifact_path):
     with open(os.path.join(artifact_path, "worker_public.txt"), "r", encoding="utf-8") as ip_f:
         for rank, ip_line in enumerate(ip_f):
             ip = ip_line.strip()[1:-1]
-            cluster_arts["worker_pub"][rank] = ip
+            cluster_arts["worker_pub"][rank + 1] = ip
 
     return cluster_arts
 
 # Pull a git repo/branch onto remote servers
-def pull(conn, repo, branch="master"):
+def pull(conn, repo, branch="master", local=True):
     """
     Performs a git pull request on a remote host
 
     :param conn: Paramiko/fabric SSH connection object
     :param str repo: Git repository to clone
     :param str branch: Specific branch to clone
+    :param bool local: Are credentials be local or on remote?
     """
 
-    # get github token from env
-    GITHUB_USER  = "calwoo"
-    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+    if local:
+        # check if a git-credentials file exists
+        cred_path = os.path.expanduser("~/.git-credentials")
+        if os.path.isfile(cred_path):
+            with open(cred_path, "r") as f:
+                cred = f.readlines()[0]
+            # set token
+            os.environ["GITHUB_TOKEN"] = cred.split(":")[2].split("@")[0]
 
-    with conn.cd("/opt/"):
-        conn.run(f"sudo git clone -b {branch} https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{repo}.git")
-    
+        # get github token from env
+        GITHUB_USER  = "calwoo"
+        GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+        with conn.cd("/opt/"):
+            conn.run(f"sudo git clone -b {branch} https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{repo}.git")
+    else:
+        with conn.cd("/opt/"):
+            conn.run(f"sudo git clone -b {branch} https://$GITHUB_USER:$GITHUB_TOKEN@github.com/{repo}.git")
+
+ 
     
 
 if __name__ == "__main__":
@@ -115,16 +131,40 @@ if __name__ == "__main__":
     logger.info("Initializing job on EC2 cluster...")
 
     # Load all IPs to SSH into
-    cluster_info = load_artifacts(args.artifacts)  
+    cluster_info = load_artifacts(args.artifacts)
+    cluster_public_ips  = {0: cluster_info["master_pub"], **cluster_info["worker_pub"]}
 
     # Establish SSH connections
-    conn = Connection(host="54.81.154.42",
-                      user=args.username,
-                      connect_kwargs={
-                        "key_filename": args.key
-                      })
+    logger.info("Establishing SSH connections to cluster nodes...")
+    cluster_conns = {rank: Connection(host=host,
+                                      user=args.username,
+                                      connect_kwargs={
+                                          "key_filename": args.key
+                                      })
+                                      for rank, host in cluster_public_ips.items()
+                    }
 
-    from pprint import pprint
-    pprint(cluster_info)
+    # pull(conn, "medivo/uwmodels", branch="cwoo-embeddings")
     
-            
+    # Run torch.distributed.launch commands all at once
+    n_nodes = len(cluster_public_ips)
+    n_gpus = 1
+
+    logger.info("Launching distributed training job...")
+    with mp.Pool(processes=n_nodes) as pool:
+        def mp_wrap(node_rank, conn):
+            cmd = pytorch_launch_cmd(n_nodes=n_nodes,
+                                     node_rank=node_rank
+                                     n_gpus=n_gpus,
+                                     master_ip=cluster_info["master_pvt"],
+                                     master_port=args.master_port,
+                                     training_path="test.py",
+                                     training_args={
+                                         "lr": 0.001,
+                                         "batch-size": 128
+                                     })
+            return conn.run(cmd)
+
+        _ = pool.map(mp_wrap, cluster_conns.items())
+
+    logger.info("Distributed training is over.")
