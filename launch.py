1eb553cd5b751490a1ad002a780d4ba2e384adb5
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-04-29 15:03:53 -0400

    replacing paramiko launch with fabric

--- launch.py
+++ launch.py
@@ -7,53 +7,36 @@ cluster.
 
 import sys
 import os
+import shutil
 import argparse
 import logging
 
-from paramiko import SSHClient, AutoAddPolicy
-from paramiko.auth_handler import AuthenticationException
+from fabric import Connection
+from fabric.group import Group, ThreadingGroup
 
 logger = logging.getLogger("launch")
 logger.setLevel(logging.INFO)
 
-class ClusterSSHClient:
-    """
-    SSH client to interact with cluster.
-    """
-    
-    def __init__(self, host, user, ssh_key_path):
-        self.host = host
-        self.user = user
-        self.ssh_key_path = ssh_key_path
-        self.client = None
 
-    def connect(self):
-        """Establish connection to cluster master"""
-        try:
-            logger.info(f"Establishing connection to {self.host}")
-            self.client = SSHClient()
-            self.client.load_system_host_keys()
-            self.client.set_missing_host_key_policy(AutoAddPolicy())
-            self.client.connect(self.host,
-                                username=self.user,
-                                key_filename=self.ssh_key_path,
-                                look_for_keys=True,
-                                timeout=5000)
-        except AuthenticationException as e:
-            logger.info("Auth failed! Please check your SSH key")
-            logger.error(e)
-            raise e
-        finally:
-            logger.info(f"Successfully connected to {self.host}!")
-            return self.client
-
-    def disconnect(self):
-        """Close SSH connection"""
-        self.client.close()
-
-
+# PyTorch distributed launch command
 def pytorch_launch_cmd(n_nodes=1, node_rank=0, n_gpus=1, master_ip="127.0.0.1",
-                       master_port=1234, training_script=None, training_args={}):
+                       master_port=1234, training_path=None, training_args={}):
+    """
+    String-wrapper for the torch.distributed.launch CLI tool; arguments to the function
+    follow the documentation:
+        https://github.com/pytorch/pytorch/blob/master/torch/distributed/launch.py
+    
+    :param int n_nodes: Number of nodes used for distributed training
+    :param int node_rank: Global rank of node for multi-node distribution
+    :param int n_gpus: Number of GPUs on node. Each GPU will run a single process
+    :param str master_ip: (Private) IP of master node in cluster. Do not put in the
+        public IP here as the worker nodes cannot communicate outside the subnet
+    :param int master_port: Port exposed on master node for distributed handshakes
+    :param str training_path: Path of experiment to run
+    :param dict training_args: Dictionary of command line args to be passed to experiment
+    :return str: Launch command to be SSHed over
+    """
+
     if training_script is None:
         raise ValueError("No training script provided")
 
@@ -70,10 +53,52 @@ def pytorch_launch_cmd(n_nodes=1, node_rank=0, n_gpus=1, master_ip="127.0.0.1",
         --node_rank={node_rank} \
         --master_addr="{master_ip}" \
         --master_port={master_port} \
-        {training_script} {training_argus}
+        {training_path} {training_argus}
     """
 
     return cmd
+
+# Load artifact files into a Python dictionary
+def load_artifacts(artifact_path):
+    """
+    Produces a Python dictionary of cluster node IPs, global rank, and number of GPUs.
+
+    :param str artifact_path: Path where cluster artifacts are stored
+    :return dict: Dictionary of cluster information
+    """
+
+    cluster_arts = {}
+    with open(os.path.join(artifact_path, "master_public.txt"), "r", encoding="utf-8") as ip_f:
+        cluster_arts["master_pub"] = ip_f.readlines()[0].strip()[1:-1]
+
+    with open(os.path.join(artifact_path, "master_private.txt"), "r", encoding="utf-8") as ip_f:
+        cluster_arts["master_pvt"] = ip_f.readlines()[0].strip()[1:-1]
+
+    cluster_arts["worker_pub"] = {}
+    with open(os.path.join(artifact_path, "worker_public.txt"), "r", encoding="utf-8") as ip_f:
+        for rank, ip_line in enumerate(ip_f):
+            ip = ip_line.strip()[1:-1]
+            cluster_arts["worker_pub"][rank] = ip
+
+    return cluster_arts
+
+# Pull a git repo/branch onto remote servers
+def pull(conn, repo, branch="master"):
+    """
+    Performs a git pull request on a remote host
+
+    :param conn: Paramiko/fabric SSH connection object
+    :param str repo: Git repository to clone
+    :param str branch: Specific branch to clone
+    """
+
+    # get github token from env
+    GITHUB_USER  = "calwoo"
+    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+    with conn.cd("/opt/"):
+        conn.run(f"sudo git clone -b {branch} https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{repo}.git")
+    
     
 
 if __name__ == "__main__":
@@ -90,32 +115,16 @@ if __name__ == "__main__":
     logger.info("Initializing job on EC2 cluster...")
 
     # Load all IPs to SSH into
-    with open(os.path.join(args.artifacts, "master_public.txt"), 
-              "r", encoding="utf-8") as ip_f:
-        master_ip = ip_f.readlines()[0].strip()[1:-1]
-
-    worker_ips = []
-    with open(os.path.join(args.artifacts, "worker_public.txt"),
-              "r", encoding="utf-8") as ip_f:
-        for ip_line in ip_f:
-            ip = ip_line.strip()[1:-1]
-            worker_ips.append(ip)
+    cluster_info = load_artifacts(args.artifacts)  
 
     # Establish SSH connections
-    ssh_client = ClusterSSHClient(host="54.145.148.154",
-                                  user=args.username,
-                                  ssh_key_path=args.key)
-    ssh_conn = ssh_client.connect()
+    conn = Connection(host="54.81.154.42",
+                      user=args.username,
+                      connect_kwargs={
+                        "key_filename": args.key
+                      })
+
+    from pprint import pprint
+    pprint(cluster_info)
     
-    # TEST
-    stdi, stdo, stde = ssh_conn.exec_command("cd what")
-    print(type(stdi))
-    print(type(stdo))
-    print(type(stde))
-
-    print(stdo.readlines())
-    print(stde.readlines())
-
-
-
-    ssh_client.disconnect()
+            
