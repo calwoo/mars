657af3e1193b474fcb8b92ad5aee2cf14dd4a95b
Calvin D. Woo <calvin.d.woo@gmail.com>
2020-05-04 19:56:35 -0400

    bump

--- provision.tf
+++ provision.tf
@@ -1,5 +1,5 @@
 data "template_file" "master_init" {
-  template = file("${path.module}/scripts/init.sh")
+  template = file("${path.module}/scripts/master/init.sh")
   vars = {
     AWS_ACCESS_KEY = var.aws_access_key
     AWS_SECRET_KEY = var.aws_secret_key
@@ -10,17 +10,17 @@ data "template_file" "master_init" {
   }
 }
 
-data "template_file" "worker_init" {
-  template = file("${path.module}/scripts/init.sh")
-  vars = {
-    AWS_ACCESS_KEY = var.aws_access_key
-    AWS_SECRET_KEY = var.aws_secret_key
-    AWS_REGION     = var.aws_region
-    GPU_HOST       = contains(["p", "g"], lower(substr(var.instance_type, 0, 1))) ? 1 : 0
-    NB_PASS        = random_password.nb_password.result
-    ROLE           = "worker"
-  }
-}
+# data "template_file" "worker_init" {
+#   template = file("${path.module}/scripts/init.sh")
+#   vars = {
+#     AWS_ACCESS_KEY = var.aws_access_key
+#     AWS_SECRET_KEY = var.aws_secret_key
+#     AWS_REGION     = var.aws_region
+#     GPU_HOST       = contains(["p", "g"], lower(substr(var.instance_type, 0, 1))) ? 1 : 0
+#     NB_PASS        = random_password.nb_password.result
+#     ROLE           = "worker"
+#   }
+# }
 
 resource "random_password" "nb_password" {
   length = 16
